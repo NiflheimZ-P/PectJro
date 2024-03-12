21fa0652e694cbd5eba1727292dfa951aab5deb7
Anawat <66070217@kmitl.ac.th>
2024-03-12 18:29:00 +0700

    change a bit

--- src/com/kmitl/pectjro/Frame/Main_Program/Admin_Mode/Sub_Windows/UserTablePage/UserEdit.java
+++ src/com/kmitl/pectjro/Frame/Main_Program/Admin_Mode/Sub_Windows/UserTablePage/UserEdit.java
@@ -13,27 +13,26 @@ import java.awt.*;
 public class UserEdit {
 	// Attribute
 	private JInternalFrame frame;
-	private JPanel main_panel, south_panel;
+	private JPanel main_panel;
 	private JProgressBar percent;
 	private Image_Resizer image;
 	private JTextField first, last, username, pass, gmail;
-	private JButton save, cancel;
 
 	// Constructor
 	public UserEdit(User_Template user) {
-		frame = new JInternalFrame("User", true, true, false);
+		frame = new JInternalFrame("User Profile", true, true, false);
 		main_panel = new JPanel(new GridBagLayout());
 		frame.setSize(400, 550);
 
 		image = new Image_Resizer(new ImageIcon(user.image), 130, 130);
 		image.setBorder(new MatteBorder(1, 1, 1, 1, Color.gray));
-		first = new JTextField(user.firstname, 13); first.setPreferredSize(new Dimension(50, 30));
-		last = new JTextField(user.lastname, 13); last.setPreferredSize(new Dimension(50, 30));
-		username = new JTextField(user.username, 13); username.setPreferredSize(new Dimension(50, 30));
-		pass = new JTextField(user.password, 13); pass.setPreferredSize(new Dimension(50, 30));
+		first = new JTextField(user.firstname, 13); first.setPreferredSize(new Dimension(50, 30)); first.setEditable(false); first.setBackground(Color.white);
+		last = new JTextField(user.lastname, 13); last.setPreferredSize(new Dimension(50, 30)); last.setEditable(false); last.setBackground(Color.white);
+		username = new JTextField(user.username, 13); username.setPreferredSize(new Dimension(50, 30)); username.setEditable(false); username.setBackground(Color.white);
+		pass = new JTextField(user.password, 13); pass.setPreferredSize(new Dimension(50, 30)); pass.setEditable(false); pass.setBackground(Color.white);
 		gmail = new JTextField(user.gmail, 29); gmail.setPreferredSize(new Dimension(50, 30));
 		gmail.setEditable(false); gmail.setBackground(Color.white);
-		gmail.setToolTipText("You can't edit this.");
+
 
 		int allPercent;
 		try { allPercent = ((user.project_ontime * 100) / user.project_done);} catch (ArithmeticException e) { allPercent = 0; }
@@ -63,12 +62,6 @@ public class UserEdit {
 		main_panel.add(percent, new Constraints(0, 8, 2, 1, 1, 0, 21, new Insets(0, 0, 20, 0)));
 
 		main_panel.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
-
-		south_panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		save = new JButton("Save"); cancel = new JButton("Cancel");
-		south_panel.add(cancel);
-		south_panel.add(save);
-		main_panel.add(south_panel, new Constraints(0, 9, 2, 1, 1, 0, 22, new Insets(0, 0, 20, 0)));
 
 		frame.add(main_panel);
 		frame.setVisible(true);
