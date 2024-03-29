33e88c66d12d833d5793e5440b236c6fdf441e75
Anawat <66070217@kmitl.ac.th>
2024-03-29 18:15:37 +0700

    Connect edit to database

---
+++ src/com/kmitl/pectjro/Frame/Main_Program/Admin_Mode/Sub_Windows/ProjectTablePage/SaveEditController.java
@@ -0,0 +1,71 @@
+package com.kmitl.pectjro.Frame.Main_Program.Admin_Mode.Sub_Windows.ProjectTablePage;
+
+import com.kmitl.pectjro.Database.Connection.DBConnect;
+import com.kmitl.pectjro.Database.ProjectTable;
+import com.kmitl.pectjro.Database.UserProjectTable;
+import com.kmitl.pectjro.Frame.Loading.Loading_dialog;
+import com.kmitl.pectjro.Frame.Templates.Project_Template;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.Date;
+
+public class SaveEditController implements ActionListener {
+	// Attribute
+	private ProjectTableModel model;
+	private ProjectEdit edit;
+
+	// Constructor
+	public SaveEditController(ProjectTableModel model, ProjectEdit edit) {
+		this.edit = edit;
+		this.model = model;
+		edit.getSave().addActionListener(this);
+		edit.getCancel().addActionListener(this);
+	}
+
+	// Listener
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(edit.getCancel())) {
+			edit.getFrame().dispose();
+		} else {
+			SwingWorker<Void, Void> update = new SwingWorker<Void, Void>() {
+				private final Loading_dialog loading = new Loading_dialog(edit.getFrame());
+				@Override
+				protected Void doInBackground() throws Exception {
+					loading.setVisible(true);
+					Connection con = DBConnect.createConnect();
+					ProjectTable project = new ProjectTable(con);
+
+					Project_Template editing = edit.getInfo();
+					editing.name = edit.getName().getText();
+					editing.description = edit.getDescription().getText();
+					editing.start = Date.valueOf(edit.getStart().getDate());
+					editing.end = Date.valueOf(edit.getEnd().getDate());
+					project.updateProject(editing);
+
+					UserProjectTable collaborators = new UserProjectTable(con);
+					for (int i: edit.getAddNew()) {
+						collaborators.addCollaborator(i, editing.id);
+					}
+
+					for (int j: edit.getDelete()) {
+						collaborators.deleteCollaborator(j, editing.id);
+					}
+
+					return null;
+				}
+
+				@Override
+				protected void done() {
+					loading.dispose();
+					edit.getFrame().dispose();
+					model.loadData();
+				}
+			};
+			update.execute();
+		}
+	}
+}
